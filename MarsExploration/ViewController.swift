806601eeb5f42fff7911b42ef68ef8317804401c
Monte Thakkar <manthan.thakkar@gmail.com>
2021-01-08 16:04:06 +0530

    Added Hello Mars Label

--- MarsExploration/ViewController.swift
+++ MarsExploration/ViewController.swift
@@ -11,7 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupConstraints()
     }
 
+    // MARK: Setup Views
+        
+    private func setupViews() {
+        view.addSubview(titleLabel)
+    }
+
+    // MARK: Setup Constraints
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    // MARK: UI Components
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello Mars"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
 }
